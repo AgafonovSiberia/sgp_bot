56a83438503383d2cab7e069177938bfffc9af0c
agafonov <agafonov@chita.ru>
2022-05-27 13:12:40 +0900

    fixes in the code

--- bot/handlers/channel/bot_chat_member.py
+++ bot/handlers/channel/bot_chat_member.py
@@ -14,7 +14,7 @@ async def joined_bot_to_channel(update: types.ChatMemberUpdated):
     """
     Добавление бота в канал
     Если канал не прописан в конфиге - бот выходит из канала
-    ** в текущем варианте подразумевается один токен на один канал
+    ** в текущем варианте подразумевается один инстанс бота на один канал
     """
     if channel_config.channel_id != update.chat.id:
         await LeaveChat(chat_id=update.chat.id)
@@ -30,6 +30,7 @@ async def joined_bot_to_channel(update: types.ChatMemberUpdated):
     """
     Выход / удаление бота из канала
     ** Тут реализуем какую-то логику в случае добавления бота в "другой" канал и его последующего выхода
+
     """
     if update.from_user.id != update.new_chat_member.user.id:
         loggers.event.info(f"Custom log - module:{__name__} - Бот был удалён из канала {update.chat.title}")

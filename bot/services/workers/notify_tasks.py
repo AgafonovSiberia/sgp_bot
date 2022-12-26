324f51e5115ded1015204bc00c3b281b30080dd6
AgafonovSiberia <agafonov@chita.ru>
2022-12-26 17:31:08 +0700

    Fix notify_tasks

--- bot/services/workers/notify_tasks.py
+++ bot/services/workers/notify_tasks.py
@@ -14,7 +14,7 @@ SENDER_TIMEOUT = 0.3
 def send_notify_for_admins(member: MemberPydantic, type_update: str):
     asyncio.run(send_notify_update(member=member, type_update=type_update))
 
-async def send_notify_update(bot: Bot, member: MemberPydantic, type_update: str):
+async def send_notify_update(member: MemberPydantic, type_update: str):
     bot = Bot(config.bot_token, parse_mode="HTML")
     text = await notify_text.get_notify_text(member, type_update)
     admins = await bot.get_chat_administrators(chat_id=config.channel_id)
